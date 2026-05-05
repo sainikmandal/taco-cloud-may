3d57417dd9534de125770634766983b96bf3cb1d
sainikm <isainikkhan@gmail.com>
2026-05-05 08:08:01 +0530

    Add may update for the Taco Cloud project to practice spring microservices and spring AI

---
+++ src/main/java/com/sainik/tacocloudmay/TacoCloudMayApplication.java
@@ -0,0 +1,13 @@
+package com.sainik.tacocloudmay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudMayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudMayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sainik/tacocloudmay/TacoCloudMayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sainik.tacocloudmay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudMayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
