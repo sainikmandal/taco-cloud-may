cc8f336b5c41c3c66bda3eed360e5175b05be391
sainikm <isainikkhan@gmail.com>
2026-05-06 05:54:50 +0530

    Add POST /orders to process and complete order session

--- src/main/java/com/sainik/tacocloudmay/controllers/OrderController.java
+++ src/main/java/com/sainik/tacocloudmay/controllers/OrderController.java
@@ -4,6 +4,7 @@ import com.sainik.tacocloudmay.models.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -17,5 +18,12 @@ public class OrderController {
     @GetMapping("/current")
     public String orderForm() {
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder order, SessionStatus sessionStatus) {
+        log.info("Order submitted: {}", order);
+        sessionStatus.setComplete();
+        return "redirect:/";
     }
 }
