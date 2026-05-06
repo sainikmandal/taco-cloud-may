508e104bf33019adce1429aeaa1e1e5700d5a78d
sainikm <isainikkhan@gmail.com>
2026-05-06 05:51:30 +0530

    Add OrderController with GET /orders/current

---
+++ src/main/java/com/sainik/tacocloudmay/controllers/OrderController.java
@@ -0,0 +1,21 @@
+package com.sainik.tacocloudmay.controllers;
+
+import com.sainik.tacocloudmay.models.TacoOrder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+@SessionAttributes("tacoOrder")
+public class OrderController {
+
+    @GetMapping("/current")
+    public String orderForm() {
+        return "orderForm";
+    }
+}
