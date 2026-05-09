faefd99a68f081ea6bb0d0757bbdab0fffc61c1d
sainikm <isainikkhan@gmail.com>
2026-05-09 12:24:52 +0530

    Annotate Ingredient with @Table and @Id

--- src/main/java/com/sainik/tacocloudmay/models/Ingredient.java
+++ src/main/java/com/sainik/tacocloudmay/models/Ingredient.java
@@ -1,9 +1,14 @@
 package com.sainik.tacocloudmay.models;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table("Ingredient")
 @Data
 public class Ingredient {
+
+    @Id
     private final String id;
     private final String name;
     private final Type type;
