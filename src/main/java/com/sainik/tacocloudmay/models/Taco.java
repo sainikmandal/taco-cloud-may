db82a7e57f4cbe5a4bbde55f86fe67335dee2ad1
sainikm <isainikkhan@gmail.com>
2026-05-09 12:24:58 +0530

    Annotate Taco with @Table, @Id and @MappedCollection for ingredients

--- src/main/java/com/sainik/tacocloudmay/models/Taco.java
+++ src/main/java/com/sainik/tacocloudmay/models/Taco.java
@@ -3,13 +3,19 @@ package com.sainik.tacocloudmay.models;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.MappedCollection;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@Table("Taco")
 @Data
 public class Taco {
 
+    @Id
     private Long id;
 
     private Date createdAt = new Date();
@@ -20,5 +26,6 @@ public class Taco {
 
     @NotNull
     @Size(min = 1, message = "You must choose at least 1 ingredient")
-    private List<Ingredient> ingredients;
+    @MappedCollection(idColumn = "taco_id")
+    private List<IngredientRef> ingredients = new ArrayList<>();
 }
