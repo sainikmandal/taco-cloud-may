9471248d9f6f469e033ad1be7c25482b9b053c56
sainikm <isainikkhan@gmail.com>
2026-05-09 12:25:06 +0530

    Extend TacoOrderRepository from CrudRepository

--- src/main/java/com/sainik/tacocloudmay/repository/TacoOrderRepository.java
+++ src/main/java/com/sainik/tacocloudmay/repository/TacoOrderRepository.java
@@ -1,7 +1,7 @@
 package com.sainik.tacocloudmay.repository;
 
 import com.sainik.tacocloudmay.models.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface TacoOrderRepository {
-    TacoOrder save(TacoOrder order);
+public interface TacoOrderRepository extends CrudRepository<TacoOrder, Long> {
 }
