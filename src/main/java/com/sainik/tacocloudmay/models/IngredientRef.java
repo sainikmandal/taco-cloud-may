9282d91240814dc276f9e0e0569ff430a90c559b
sainikm <isainikkhan@gmail.com>
2026-05-09 12:24:55 +0530

    Annotate IngredientRef with @Table

--- src/main/java/com/sainik/tacocloudmay/models/IngredientRef.java
+++ src/main/java/com/sainik/tacocloudmay/models/IngredientRef.java
@@ -1,7 +1,9 @@
 package com.sainik.tacocloudmay.models;
 
 import lombok.Data;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table("Taco_Ingredients")
 @Data
 public class IngredientRef {
     private final String ingredientId;
